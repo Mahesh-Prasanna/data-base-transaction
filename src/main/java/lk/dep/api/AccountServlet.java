1d01bc100fada43ac04bc067444f05a00986f300
Mahesh Prasanna <damp.dassanayake@gmail.com>
2022-11-14 06:43:31 +0530

    Finalize Create Account

--- src/main/java/lk/dep/api/AccountServlet.java
+++ src/main/java/lk/dep/api/AccountServlet.java
@@ -5,6 +5,7 @@ import jakarta.json.Json;
 import jakarta.json.JsonException;
 import jakarta.json.JsonObject;
 import jakarta.json.bind.JsonbBuilder;
+import jakarta.json.bind.JsonbException;
 import jakarta.json.stream.JsonParser;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -16,6 +17,11 @@ import lk.dep.dto.AccountDTO;
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.StringReader;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.UUID;
 
 @WebServlet(name = "accountServlet", urlPatterns = "/accounts/*", loadOnStartup = 0)
 public class AccountServlet extends HttpServlet {
@@ -42,7 +48,33 @@ public class AccountServlet extends HttpServlet {
             resp.sendError(HttpServletResponse.SC_NOT_IMPLEMENTED);
         }
     }
-    private void createAccount(AccountDTO accountDTO, HttpServletResponse response){
+    private void createAccount(AccountDTO accountDTO, HttpServletResponse response) throws IOException {
+        try (Connection connection = pool.getConnection()) {
+            if (accountDTO.getName() == null || !accountDTO.getName().matches("[A-Za-z ]+")){
+                throw new JsonbException("Invalid Account Holder Name");
+            } else if (accountDTO.getAddress() == null || accountDTO.getAddress().isBlank()) {
+                throw new JsonbException("Invalid Account Holder name");
+            }
 
+            accountDTO.setAccount(UUID.randomUUID().toString());
+            accountDTO.setBalance(BigDecimal.ZERO);
+
+            PreparedStatement stm = connection.
+                    prepareStatement("INSERT INTO Account (account_number, holder_name, holder_address) VALUES (?, ?, ?)");
+            stm.setString(1, accountDTO.getAccount());
+            stm.setString(2, accountDTO.getAccount());
+            stm.setString(3, accountDTO.getAccount());
+            if (stm.executeUpdate() == 1){
+                response.setStatus(HttpServletResponse.SC_CREATED);
+                response.setContentType("application/json");
+                JsonbBuilder.create().toJson(accountDTO, response.getWriter());
+            }else {
+                throw new SQLException("Something Went Wrong, Try agin");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+        }
     }
 }
